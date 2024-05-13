705d6855d8f265f0c8a6d5efc7456ea450cb6924
Ruan Wallace <ruanwallace2k@gmail.com>
2024-05-13 02:07:02 -0300

    fs module added

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const urlBase = 'https://fundamentus.com.br/detalhes.php?papel=';
 
 const assembleTheUrl = () => {
     let paper = 'PETR4';
-    
+
     return `${urlBase}${paper}`;
 };
 
 const pageRequest = () => {
     axios.get(assembleTheUrl())
         .then(response => {
-            console.log(response.data);
+            return response.data;
         });
 };
