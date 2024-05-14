9df949332cd269501d45120362d702e17b1b0cd0
Ruan Wallace <ruanwallace2k@gmail.com>
2024-05-14 02:47:11 -0300

    added writePage function

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,24 @@
 const axios = require('axios');
 const fs = require('fs');
 
+let paper = 'PETR4';
 const urlBase = 'https://fundamentus.com.br/detalhes.php?papel=';
 
-const assembleTheUrl = () => {
-    let paper = 'PETR4';
-
-    return `${urlBase}${paper}`;
+const assembleTheUrl = (url, ticket) => {
+    return `${url}${ticket}`;
 };
 
-const pageRequest = () => {
-    axios.get(assembleTheUrl())
-        .then(response => {
-            return response.data;
-        });
+const pageRequest = (url, ticket) => {
+    return axios.get(assembleTheUrl(url, ticket))
+    .then(response => response.data);    
 };
+
+const writePage = async () => {
+    let fileName = `src/cache/${paper}.html`;
+    let html = await pageRequest(urlBase, paper);
+
+    fs.writeFile(fileName, html, (err) => {
+        if (err) throw err;
+        console.log("PEGOU!");
+    });
+}
